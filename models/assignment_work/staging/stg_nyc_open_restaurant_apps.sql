caeab135836c8e36de0ba4a574b0097349bd252e
Wenli Li <wenli.li1@baruchmail.cuny.edu>
2026-03-22 23:37:13 +0000

    change yml

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -1,4 +1,3 @@
--- Clean and standardize 311 DOT service request data
 -- One row per service request
 
 WITH source AS (
@@ -10,85 +9,52 @@ cleaned AS (
        -- Get all columns from source, except ones we're transforming below
        -- To do cleaning on them or explicitly cast them as types just in case
        * EXCEPT (
-           unique_key,
-           created_date,
-           closed_date,
-           agency,
-           agency_name,
-           complaint_type,
-           descriptor,
-           status,
-           incident_zip,
+           objectid,
+           time_of_submission,
+           zip,
            borough,
-           incident_address,
-           street_name,
-           cross_street_1,
-           cross_street_2,
+           business_address,
+           street,
            latitude,
            longitude,
-           open_data_channel_type
+           qualify_alcohol
        ),
 
        -- Identifiers
-       CAST(unique_key AS STRING) AS request_id,
+       CAST(objectid AS STRING) AS objectid,
 
        -- Date/Time
-       CAST(created_date AS TIMESTAMP) AS created_date,
+       CAST(time_of_submission AS TIMESTAMP) AS time_of_submission,
        CAST(closed_date AS TIMESTAMP) AS closed_date,
 
-       -- Request details
-       CAST(agency AS STRING) AS agency,
-       CAST(agency_name AS STRING) AS agency_name,
-       CAST(complaint_type AS STRING) AS complaint_type,
-       CAST(descriptor AS STRING) AS descriptor,
-       UPPER(TRIM(CAST(status AS STRING))) AS status,
+       --qualify_alcohol
+       CASE 
+            WHEN LOWER(TRIM(qualify_alcohol)) = 'yes' THEN TRUE 
+            ELSE FALSE 
+        END AS qualify_alcohol,
 
        -- Location - clean zip code, handling several common zip code data problems
        CASE
-           WHEN UPPER(TRIM(CAST(incident_zip AS STRING))) IN ('N/A', 'NA') THEN NULL
-           WHEN UPPER(TRIM(CAST(incident_zip AS STRING))) = 'ANONYMOUS' THEN 'Anonymous'
-           WHEN LENGTH(CAST(incident_zip AS STRING)) = 5 THEN CAST(incident_zip AS STRING)
-           WHEN LENGTH(CAST(incident_zip AS STRING)) = 9 THEN CAST(incident_zip AS STRING)
-           WHEN LENGTH(CAST(incident_zip AS STRING)) = 10
-               AND REGEXP_CONTAINS(CAST(incident_zip AS STRING), r'^\d{5}-\d{4}')
-           THEN CAST(incident_zip AS STRING)
-           ELSE NULL
-       END AS incident_zip,
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) IN ('N/A', 'NA', '') THEN NULL
+           ELSE LPAD(TRIM(CAST(zip AS STRING)), 5, '0')
+       END AS zip,
 
-       -- Location - standardized borough, just in case
-       CASE
-           WHEN UPPER(TRIM(borough)) IN ('MANHATTAN', 'NEW YORK COUNTY') THEN 'Manhattan'
-           WHEN UPPER(TRIM(borough)) IN ('BRONX', 'THE BRONX') THEN 'Bronx'
-           WHEN UPPER(TRIM(borough)) IN ('BROOKLYN', 'KINGS COUNTY') THEN 'Brooklyn'
-           WHEN UPPER(TRIM(borough)) IN ('QUEENS', 'QUEEN', 'QUEENS COUNTY') THEN 'Queens'
-           WHEN UPPER(TRIM(borough)) IN ('STATEN ISLAND', 'RICHMOND COUNTY') THEN 'Staten Island'
-           ELSE 'UNKNOWN or CITYWIDE'
-       END AS borough,
-
-       CAST(incident_address AS STRING) AS incident_address,
-       CAST(street_name AS STRING) AS street_name,
-       CAST(cross_street_1 AS STRING) AS cross_street_1,
-       CAST(cross_street_2 AS STRING) AS cross_street_2,
+       CAST(business_address AS STRING) AS business_address,
+       CAST(street AS STRING) AS street,
        CAST(latitude AS DECIMAL) AS latitude,
        CAST(longitude AS DECIMAL) AS longitude,
 
-       -- Clearer column name as well for this one
-       CAST(open_data_channel_type AS STRING) AS method_of_submission,
-
        -- Metadata
        CURRENT_TIMESTAMP() AS _stg_loaded_at
-
    FROM source
 
    -- Filters
-   WHERE (agency = 'DOT' OR agency_name LIKE '%Transportation%')
-   AND unique_key IS NOT NULL
-   AND created_date IS NOT NULL
-   AND CAST(created_date AS DATE) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 YEAR)
+   WHERE objectid IS NOT NULL
+   AND time_of_submission IS NOT NULL
    AND borough IS NOT NULL
 
    -- Deduplicate
-   QUALIFY ROW_NUMBER() OVER (PARTITION BY unique_key ORDER BY created_date DESC) = 1
+   QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY time_of_submission DESC) = 1
 )
 
 SELECT * FROM cleaned
