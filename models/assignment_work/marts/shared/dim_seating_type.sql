8841da3919abb4b5f88a03edb9fdd36c38fc142b
Wenli Li <wenli.li1@baruchmail.cuny.edu>
2026-03-23 00:18:37 +0000

    edit

--- models/assignment_work/marts/shared/dim_seating_type.sql
+++ models/assignment_work/marts/shared/dim_seating_type.sql
@@ -4,8 +4,15 @@ WITH seating_types AS (
        seating_interest_sidewalk AS seating_interest,
 --TODO: Replace this comment with a CASE WHEN .. statement that handles the different possibilities for approved_for_sidewalk_seating and approved_for_roadway_seating in the data
 --NOTE: The final result we want to select here is two boolean columns (TRUE or FALSE values in them), one column approved_for_sidewalk (TRUE or FALSE value), and one column approved_for_roadway 
-      AS approved_for_roadway
-   FROM --TODO: reference the appropriate staging table!
+    CASE 
+        WHEN LOWER(TRIM(CAST(approved_for_roadway_seating AS STRING))) IN ('yes', 'true') THEN TRUE 
+        ELSE FALSE 
+    END AS approved_for_roadway,
+    CASE 
+        WHEN LOWER(TRIM(CAST(approved_for_sidewalk_seating AS STRING))) IN ('yes', 'true') THEN TRUE 
+        ELSE FALSE 
+    END AS approved_for_sidewalk
+   FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE seating_interest_sidewalk IS NOT NULL
 ),
 seating_dimension AS (
@@ -18,7 +25,9 @@ seating_dimension AS (
 
        -- TODO: fill in the rest of this SELECT statement
        --  based on the dimensional model!
-
+        seating_interest,
+        approved_for_sidewalk,
+        approved_for_roadway
    FROM seating_types
 )
 
